bac8aa840658fdaff008d155c63f2ddbd048d9f9
Nguyen Thu Thuy <haducc246@gmail.com>
2021-03-24 23:21:56 +0700

    Refract code

---
+++ user/partials/html_header.php
@@ -0,0 +1,25 @@
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" name="viewport">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-touch-fullscreen" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<title><?=$pageTitle?></title>
+<link rel="icon" type="image/x-icon" href="/assets/img/logo.png"/>
+<link rel="icon" href="/assets/img/logo.png" type="image/png" sizes="16x16">
+<link rel="stylesheet" href="/assets/vendor/pace/pace.css">
+<script src="/assets/vendor/pace/pace.min.js"></script>
+<!--vendors-->
+<link rel="stylesheet" type="text/css" href="/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker3.min.css">
+<link rel="stylesheet" type="text/css" href="/assets/vendor/jquery-scrollbar/jquery.scrollbar.css">
+<link rel="stylesheet" href="/assets/vendor/select2/css/select2.min.css">
+<link rel="stylesheet" href="/assets/vendor/jquery-ui/jquery-ui.min.css">
+<link rel="stylesheet" href="/assets/vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" href="/assets/vendor/timepicker/bootstrap-timepicker.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,600" rel="stylesheet">
+<!--Material Icons-->
+<link rel="stylesheet" type="text/css" href="/assets/fonts/materialdesignicons/materialdesignicons.min.css">
+<!--Material Icons-->
+<link rel="stylesheet" type="text/css" href="/assets/fonts/feather/feather-icons.css">
+<!--Bootstrap + atmos Admin CSS-->
+<link rel="stylesheet" type="text/css" href="/assets/css/atmos.min.css">
+<!-- Additional library for page -->
