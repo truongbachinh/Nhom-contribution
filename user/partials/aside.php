70e8a674fce69c1efe41c794ef93d5e0cb0c0d35
Nguyen Thu Thuy <haducc246@gmail.com>
2021-03-25 00:52:11 +0700

    aside

--- user/partials/aside.php
+++ user/partials/aside.php
@@ -30,77 +30,72 @@
                     <li class="menu-item ">
                         <a href="#" class="open-dropdown menu-link">
                             <span class="menu-label">
-                                <span class="menu-name">Dashboard
-                                </span>
+                                <span class="menu-name">Dashboard</span>
                             </span>
-                            <span class="menu-icon">
-                        <i class="icon-placeholder fe fe-edit "></i>
-                    </span>
+                            <span class="menu-icon"><i class="icon-placeholder fe fe-edit "></i>
+                             </span>
                         </a>
                     </li>
                     <li class="menu-item active opened">
                         <a href="#" class="open-dropdown menu-link">
-                    <span class="menu-label">
-                        <span class="menu-name">Manage System
-                            <span class="menu-arrow"></span>
-                        </span>
-
-                    </span>
+                            <span class="menu-label">
+                                <span class="menu-name">Manage System
+                                 <span class="menu-arrow"></span> </span>
+                             </span>
                             <span class="menu-icon">
-                        <i class="mdi mdi-buffer mdi-24px "></i>
-                    </span>
+                                <i class="mdi mdi-buffer mdi-24px "></i>
+                            </span>
                         </a>
                         <!--submenu-->
                         <ul class="sub-menu" style="display: block;">
-
                             <li class="menu-item ">
                                 <a href="#" class=" menu-link">
-                            <span class="menu-label">
-                                <span class="menu-name">Manage Account</span>
-                            </span>
+                                    <span class="menu-label">
+                                        <span class="menu-name">Manage Account</span>
+                                    </span>
                                     <span class="menu-icon">
-                                <i class="mdi mdi-account-multiple mdi-24px "></i>
-                            </span>
+                                        <i class="mdi mdi-account-multiple mdi-24px "></i>
+                                    </span>
                                 </a>
                             </li>
                             <li class="menu-item">
                                 <a href="./admin/manage_faculties.php" class=" menu-link">
-                            <span class="menu-label">
-                                <span class="menu-name">Manage Faculty</span>
-                            </span>
+                                     <span class="menu-label">
+                                        <span class="menu-name">Manage Faculty</span>
+                                         </span>
                                     <span class="menu-icon">
-                                <i class="mdi mdi-briefcase mdi-24px "></i>
-                            </span>
+                                         <i class="mdi mdi-briefcase mdi-24px "></i>
+                                    </span>
                                 </a>
                             </li>
                             <li class="menu-item">
                                 <a href="./admin/manage-topics.php" class=" menu-link">
-                            <span class="menu-label">
-                                <span class="menu-name">Manage Topic</span>
-                            </span>
+                                    <span class="menu-label">
+                                        <span class="menu-name">Manage Topic</span>
+                                    </span>
                                     <span class="menu-icon">
-                                <i class="mdi mdi-book-open-variant mdi-24px "></i>
-                            </span>
+                                        <i class="mdi mdi-book-open-variant mdi-24px "></i>
+                                    </span>
                                 </a>
                             </li>
                             <li class="menu-item">
                                 <a href="./admin/manage_users.php" class=" menu-link">
-                            <span class="menu-label">
-                                <span class="menu-name">Manage User</span>
-                            </span>
+                                    <span class="menu-label">
+                                         <span class="menu-name">Manage User</span>
+                                    </span>
                                     <span class="menu-icon">
-                                <i class="mdi mdi-book-open-variant mdi-24px "></i>
-                            </span>
+                                        <i class="mdi mdi-book-open-variant mdi-24px "></i>
+                                    </span>
                                 </a>
                             </li>
                             <li class="menu-item">
                                 <a href="./admin/manage_articles.php" class=" menu-link">
-                            <span class="menu-label">
-                                <span class="menu-name">Manage Article</span>
-                            </span>
+                                    <span class="menu-label">
+                                        <span class="menu-name">Manage Article</span>
+                                     </span>
                                     <span class="menu-icon">
-                                <i class="mdi mdi-file mdi-24px "></i>
-                            </span>
+                                         <i class="mdi mdi-file mdi-24px "></i>
+                                    </span>
                                 </a>
                             </li>
                         </ul>
@@ -153,10 +148,10 @@
                     </li>
                 <?php elseif ($currentUser['role'] === "manager-coordinator"): ?>
                     <li class="menu-item active ">
-                        <a href="/user/student/" class="menu-link">
+                        <a href="#" class="menu-link">
                             <span class="menu-label">
                                 <span class="menu-name">
-                                    Home
+                                    Home Page
                                 </span>
                             </span>
                             <span class="menu-icon">
@@ -165,7 +160,7 @@
                         </a>
                     </li>
                     <li class="menu-item ">
-                        <a href="/user/student/view_detail_faculty.php" class="menu-link">
+                        <a href="#" class="menu-link">
                             <span class="menu-label">
                                 <span class="menu-name">My Faculty
                                 </span>
@@ -176,6 +171,29 @@
                         </a>
                     </li>
                 <?php elseif ($currentUser['role'] === "manager-marketing"): ?>
+                    <li class="menu-item active ">
+                        <a href="#" class="menu-link">
+                            <span class="menu-label">
+                                <span class="menu-name">
+                                    Home Page
+                                </span>
+                            </span>
+                            <span class="menu-icon">
+                                <i class="icon-placeholder fe fe-activity "></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="menu-item ">
+                        <a href="#" class="menu-link">
+                            <span class="menu-label">
+                                <span class="menu-name">Manage Faculty
+                                </span>
+                            </span>
+                            <span class="menu-icon">
+                                <i class="icon-placeholder fe fe-folder"></i>
+                            </span>
+                        </a>
+                    </li>
 
                 <?php endif ?>
 
